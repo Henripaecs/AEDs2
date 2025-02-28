499976475420c284f2dda286842b60372ca9e894
Henrique Paes de Carvalho <163349410+Henripaecs@users.noreply.github.com>
2025-02-27 23:38:24 -0300

    Update SomaDigitos.java

--- Q08/SomaDigitos.java
+++ Q08/SomaDigitos.java
@@ -1,22 +1,25 @@
-package Q08;
+//package Q08;
 import java.util.Scanner;
 
 public class SomaDigitos {
-    public static int somar(String n){
-        if(n == 0 || n == 1){
-            return 1;
+    public static int somar(String n, int aux){
+        if(aux == n.length()){
+            return 0;
         }
-        return n + somar(n-1);
+        return Character.getNumericValue(n.charAt(aux)) + somar(n, aux + 1);
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while(true){
-            Scanner entrada = scanner.nextLine();
+        while(true){            
+            String entrada = scanner.nextLine();
+
             if(entrada.equals("FIM")){
                 break;
             }
-            System.out.println(somar(entrada));
+            int soma = somar(entrada, 0);
+            System.out.println(soma);
         }
+        scanner.close();
     }
 }
