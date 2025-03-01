33467db8957bf59e5634f3b2d0c821f9fa33a8e4
Henrique Paes de Carvalho <163349410+Henripaecs@users.noreply.github.com>
2025-03-01 11:12:31 -0300

    Create SomaDigitos.java

---
+++ TP1/Q08/SomaDigitos.java
@@ -0,0 +1,25 @@
+package Q08;
+import java.util.Scanner;
+
+public class SomaDigitos {
+    public static int somar(String n, int aux){
+        if(aux == n.length()){
+            return 0;
+        }
+        return Character.getNumericValue(n.charAt(aux)) + somar(n, aux + 1);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){            
+            String entrada = scanner.nextLine();
+
+            if(entrada.equals("FIM")){
+                break;
+            }
+            int soma = somar(entrada, 0);
+            System.out.println(soma);
+        }
+        scanner.close();
+    }
+}
