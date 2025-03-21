adfe3bf09f40b41988f509fcd585505455e66757
Henrique Paes de Carvalho <163349410+Henripaecs@users.noreply.github.com>
2025-03-21 11:48:34 -0300

    Create Aquecimento.java

---
+++ Lab/Aquecimento.java
@@ -0,0 +1,28 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class Aquecimento {
+
+    public static int upper(String arr){
+        char[] l = arr.toCharArray();
+        int cont = 0;
+        for (int i = 0; i < arr.length(); i++) {
+            if (Character.isUpperCase(l[i])) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            String str = scanner.nextLine();
+            if (str.equals("FIM")) {
+                break;
+            }
+            System.out.println(upper(str));
+        } while (true);
+    }
+}
