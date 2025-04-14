964b121c10cacbf3e34f462e586e25ad8084c575
Henrique Paes de Carvalho <163349410+Henripaecs@users.noreply.github.com>
2025-04-14 14:35:01 -0300

    Show.java

--- TP2/show.java
+++ TP2/show.java
@@ -1,147 +1,170 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
 
-public class show {
-    private String showId;    
+public class Show implements Cloneable {
+    private String show_id;
     private String type;
     private String title;
-    private String diretor;
+    private String director;
     private String[] cast;
-    private String contry;
+    private String country;
     private Date date_added;
     private int release_year;
-    private String duration;
     private String rating;
+    private String duration;
     private String[] listed_in;
 
-    public show(String showId, String type, String title, String diretor, String[] cast, String contry, Date date_added, int release_year, String rating, String duration, String[] lusted_in){
-        this.showId = showId;
-        this.type = type;
-        this.title = title;
-        this.diretor = diretor;
-        this.cast = cast;
-        this.contry = contry;
-        this.date_added = date_added;
-        this.release_year = release_year;
-        this.rating = rating;
-        this. duration = duration;
-        this.listed_in = listed_in;
-    }
-    public show(){
-        this.showId = "NaN";
+    public Show() {
+        this.show_id = "NaN";
         this.type = "NaN";
         this.title = "NaN";
-        this.diretor =package Tp2; "NaN";
+        this.director = "NaN";
         this.cast = new String[]{"NaN"};
-        this.contry = "NaN";
+        this.country = "NaN";
         this.date_added = null;
         this.release_year = -1;
         this.rating = "NaN";
-        this. duration = "NaN";
+        this.duration = "NaN";
         this.listed_in = new String[]{"NaN"};
-
     }
 
-    public String getShowId(){
-        return showId;
-    }
-    public void setShowId(String showId){
-        this.showId = showId;
-    }
-    public String getType(){
-        return type;
-    }
-    public void setType(String type){
+    public Show(String show_id, String type, String title, String director, String[] cast, String country,
+                Date date_added, int release_year, String rating, String duration, String[] listed_in) {
+        this.show_id = show_id;
         this.type = type;
-    }
-    public String getTitle(){
-        return title;
-    }
-    public void setTitle(String title){
         this.title = title;
-    }
-    public String getDiretor(){
-        return diretor;
-    }
-    public void setDiretor(String diretor){
-        this.diretor = diretor;
-    }
-    public String[] getCast(){
-        return cast;
-    }
-    public void setCast(String[] cast){
+        this.director = director;
         this.cast = cast;
-    }
-    public String getContry(){
-        return contry;
-    }
-    public void setContry(String contry){
-        this.contry = contry;
-    }
-    public Date getDate_added(){
-        return date_added;
-    }
-    public void setDate_added(Date date_added){
+        this.country = country;
         this.date_added = date_added;
-    }
-    public int getRelease_year(){
-        return release_year;
-    }
-    public void seteRelease_year(int release_year){
         this.release_year = release_year;
-    }
-    public String getRating(){
-        return rating;
-    }
-    public void seteRating(String rating){
         this.rating = rating;
-    }
-    public String getDuration(){
-        return duration;
-    }
-    public void setDuration(String duration){
         this.duration = duration;
-    }
-    public String[] getListed_in(){
-        return listed_in;
-    }
-    public void setListed_in(String[] listed_in){
         this.listed_in = listed_in;
     }
-    
-    public show clone(){
-        show cloneShow = new show();
-        cloneShow.showId = this.showId; 
-        cloneShow.type = this.type;
-        cloneShow.title = this.title;
-        cloneShow.diretor = this.diretor;
-        cloneShow.cast = this.cast;
-        cloneShow.contry = this.contry;
-        cloneShow.date_added = this.date_added;
-        cloneShow.release_year = this.release_year;
-        cloneShow.rating = this.rating;
-        cloneShow.duration = this.duration;
-        cloneShow.listed_in = this.listed_in;
-        return cloneShow;
-    }
-    public void print(){
-        System.out.println(this.showId + "##" + this.type + "##" + this.title + "##" + this.diretor + "##"
-        + this.cast + "##" + this.contry + "##" + this.date_added + "##" + this.release_year + "##" + this.rating + "##"+ this.duration + "##"
-        + this.listed_in);
-    }
-    public void ler(String imput){
-        String[] parts = imput.split(" ## ");
-        this.showId = parts[0];
-        this.type = parts[1];
-        this.title = parts[2];
-        this.diretor = parts[3];
-        this.cast = parts[4].split (", ");
-        this.contry = parts[5];
-        this.date_added = new Date();
-        this.release_year = Integer.parseInt(parts[7]);
-        this.rating = parts[8];
-        this.duration = parts[9];
-        this.listed_in = parts[10].split(", ");
 
+    public String getShow_id() { return show_id; }
+    public void setShow_id(String show_id) { this.show_id = show_id; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDirector() { return director; }
+    public void setDirector(String director) { this.director = director; }
+
+    public String[] getCast() { return cast; }
+    public void setCast(String[] cast) { this.cast = cast; }
+
+    public String getCountry() { return country; }
+    public void setCountry(String country) { this.country = country; }
+
+    public Date getDate_added() { return date_added; }
+    public void setDate_added(Date date_added) { this.date_added = date_added; }
+
+    public int getRelease_year() { return release_year; }
+    public void setRelease_year(int release_year) { this.release_year = release_year; }
+
+    public String getRating() { return rating; }
+    public void setRating(String rating) { this.rating = rating; }
+
+    public String getDuration() { return duration; }
+    public void setDuration(String duration) { this.duration = duration; }
+
+    public String[] getListed_in() { return listed_in; }
+    public void setListed_in(String[] listed_in) { this.listed_in = listed_in; }
+
+    //  clone
+    public Show clone() {
+        return new Show(show_id, type, title, director, cast.clone(), country, date_added, release_year, rating, duration, listed_in.clone());
+    }
+
+    // imprimir
+    public void imprimir() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH);
+        String dataStr = (date_added != null) ? sdf.format(date_added) : "NaN";
+    
+        //ordenar
+        String[] castOrdenado = cast.clone();
+        Arrays.sort(castOrdenado, String.CASE_INSENSITIVE_ORDER);
+    
+        //tirar ("") titulo
+        String titleLimpo = title.replaceAll("\"", "");
+
+        //tirar ("") cast
+        for (int i = 0; i < castOrdenado.length; i++){
+            castOrdenado[i] = castOrdenado[i].replaceAll("\"", "");
+        } 
+    
+        System.out.println("=> " + show_id + " ## " + titleLimpo + " ## " + type + " ## " + director +
+                " ## " + Arrays.toString(castOrdenado) + " ## " + country + " ## " + dataStr + " ## " +
+                release_year + " ## " + rating + " ## " + duration + " ## " + Arrays.toString(listed_in) + " ##");
     }
     
+
+    public void ler(String linha) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy");
+        String[] campos = new String[12];
+
+        String[] partes = linha.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+        for (int i = 0; i < campos.length; i++) {
+            campos[i] = (i < partes.length && !partes[i].trim().isEmpty()) ? partes[i].trim().replaceAll("^\"|\"$", "") : "NaN";
+        }
+
+        this.show_id = campos[0];
+        this.type = campos[1];
+        this.title = campos[2];
+        this.director = campos[3];
+        this.cast = campos[4].equals("NaN") ? new String[]{"NaN"} : campos[4].split(", ");
+        this.country = campos[5];
+        try {
+            this.date_added = campos[6].equals("NaN") ? null : sdf.parse(campos[6]);
+        } catch (ParseException e) {
+            this.date_added = null;
+        }
+        this.release_year = campos[7].equals("NaN") ? -1 : Integer.parseInt(campos[7]);
+        this.rating = campos[8];
+        this.duration = campos[9];
+        this.listed_in = campos[10].equals("NaN") ? new String[]{"NaN"} : campos[10].split(", ");
+    }
+    public static void main(String[] args) throws IOException {
+        Map<String, String> dadosCSV = new HashMap<>();
+        //BufferedReader br = new BufferedReader(new FileReader("./disneyplus.csv"));//maquina
+        BufferedReader br = new BufferedReader(new FileReader("/tmp/disneyplus.csv"));//verde 
+
+        br.readLine();
+
+        String linha;
+        while ((linha = br.readLine()) != null) {
+            String id = linha.split(",")[0];
+            dadosCSV.put(id, linha);
+        }
+        br.close();
+
+        Scanner sc = new Scanner(System.in);
+        String entrada;
+
+        while (!(entrada = sc.nextLine()).equals("FIM")) {
+            if (dadosCSV.containsKey(entrada)) {
+                Show show = new Show();
+                show.ler(dadosCSV.get(entrada));
+                show.imprimir();
+            } else {
+                System.out.println("=> NaN ## NaN ## NaN ## NaN ## [NaN] ## NaN ## NaN ## -1 ## NaN ## NaN ## [NaN] ##");
+            }
+        }
+
+        sc.close();
+    }
 }
