41ca71833f46d418162029d28e2f01afd21cc325
Henrique Paes de Carvalho <163349410+Henripaecs@users.noreply.github.com>
2025-05-15 14:42:35 -0300

    Create calcular.java

---
+++ Beecrowd/calcular.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class calcular {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US); 
+        
+        while(sc.hasNextLine()){
+            int cod1 = sc.nextInt();
+            int qtd1 = sc.nextInt();
+            double preco1 = sc.nextDouble();
+
+            int cod2 = sc.nextInt();
+            int qtd2 = sc.nextInt();
+            double preco2 = sc.nextDouble();
+
+            double total = qtd1 * preco1 + qtd2 * preco2;
+
+            System.out.printf("VALOR A PAGAR: R$ %.2f%n", total);
+
+            sc.close();
+        }
+        
+    }
+}
